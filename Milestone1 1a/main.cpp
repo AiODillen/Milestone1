ac24d4f8c604ccdd4f6d986958ee3e5a829e2bbb
Niklas <niklas@Niklass-MacBook-Air.local>
2018-10-19 11:59:04 +0200

    Initial Commit

---
+++ Milestone1 1a/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Milestone1 1a
+//
+//  Created by Niklas on 19.10.18.
+//  Copyright © 2018 Niklas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
